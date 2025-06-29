b3bc3bd056d3b9bf1c095c9917458d8ac3db7e25
riqueamais <henrique.quintino99@gmail.com>
2025-06-29 20:46:23 -0300

    feat: initialize restaurant API with TypeScript, Express, and dotenv
    - Added package.json to manage dependencies and scripts
    - Created server.ts to set up a basic Express server
    - Added tsconfig.json for TypeScript configuration

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import "dotenv/config";
+import express from "express";
+
+const PORT = process.env.PORT || 3333;
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
